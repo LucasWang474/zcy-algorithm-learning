b094fa42871de5ed9089db3c5ee53193171367da
Lucas Wang <474.lucas.wang@gmail.com>
2022-12-18 16:42:14 +0800

    feat: add basic sorts

---
+++ 01-basic/class01/Code01_SelectionSort.ts
@@ -0,0 +1,61 @@
+function SelectionSort(arr: number[]): number[] {
+  for (let i = 0; i < arr.length; i++) {
+    let minI = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minI]) {
+        minI = j;
+      }
+    }
+
+    if (minI !== i) {
+      swap(arr, i, minI);
+    }
+  }
+  return arr;
+}
+
+function swap(arr: number[], i: number, j: number) {
+  let temp;
+  temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function randomArr(len = 100, L = -1000, R = 1000): number[] {
+  const arr = [];
+  for (let i = 0; i < len; i++) {
+    arr.push(Math.trunc(Math.random() * (R - L) + L));
+  }
+  return arr;
+}
+
+function arrEquals(arr1: any[], arr2: any[]): boolean {
+  if (arr1?.length !== arr2?.length) return false;
+  for (let i = 0; i < arr1?.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+
+function test(times = 1000, len = 100) {
+  const label = `SelectionSort: times: ${times}, len: ${len}`;
+  console.time(label);
+  for (let i = 0; i < times; i++) {
+    const input = randomArr(len);
+    const expected = input.slice().sort((a, b) => a - b);
+    const actual = SelectionSort(input.slice());
+    if (!arrEquals(expected, actual)) {
+      console.assert(false, {
+        input,
+        expected,
+        actual,
+      });
+      return;
+    }
+  }
+  console.timeEnd(label);
+}
+
+const times = +process.argv[2] || 1000;
+const len = +process.argv[3] || 100;
+test(times, len);
---
+++ 01-basic/class01/Code03_InsertionSort.ts
@@ -0,0 +1,54 @@
+function InsertionSort(arr: number[]): number[] {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j > 0 && arr[j - 1] > arr[j]; j--) {
+      swap(arr, j - 1, j);
+    }
+  }
+  return arr;
+}
+
+function swap(arr: number[], i: number, j: number) {
+  let temp;
+  temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+function randomArr(len = 100, L = -1000, R = 1000): number[] {
+  const arr = [];
+  for (let i = 0; i < len; i++) {
+    arr.push(Math.trunc(Math.random() * (R - L) + L));
+  }
+  return arr;
+}
+
+function arrEquals(arr1: any[], arr2: any[]): boolean {
+  if (arr1?.length !== arr2?.length) return false;
+  for (let i = 0; i < arr1?.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+
+function test(times = 1000, len = 100) {
+  const label = `InsertionSort: times: ${times}, len: ${len}`;
+  console.time(label);
+  for (let i = 0; i < times; i++) {
+    const input = randomArr(len);
+    const expected = input.slice().sort((a, b) => a - b);
+    const actual = InsertionSort(input.slice());
+    if (!arrEquals(expected, actual)) {
+      console.assert(false, {
+        input,
+        expected,
+        actual,
+      });
+      return;
+    }
+  }
+  console.timeEnd(label);
+}
+
+const times = +process.argv[2] || 1000;
+const len = +process.argv[3] || 100;
+test(times, len);
