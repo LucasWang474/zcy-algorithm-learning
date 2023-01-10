c13b39607f8c8c56652af5d1ec50da4209374a7c
Lucas Wang <474.lucas.wang@gmail.com>
2023-01-11 03:29:32 +0800

    feat: add analysis code

--- 01-basic/class05/Code02_PartitionAndQuickSort.ts
+++ 01-basic/class05/Code02_PartitionAndQuickSort.ts
@@ -39,3 +39,57 @@ function partition(nums: number[], pivot: number, lo: number, hi: number) {
 
   return [lt, gt];
 }
+
+function isSameArray(arr1: any[], arr2: any[]) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+
+function randomArray(n: number, min: number, max: number): number[] {
+  const arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(Math.floor(Math.random() * (max - min + 1)) + min);
+  }
+  return arr;
+}
+
+function test() {
+  // Time test
+  let prevTotalTime = 0,
+    totalTime = 0;
+  const times = 1000;
+  for (let N = 10; N <= Number.MAX_VALUE; N *= 2) {
+    const min = -10000,
+      max = 10000;
+    const timeStart = Date.now();
+
+    for (let i = 0; i < times; i++) {
+      const arr = randomArray(N, min, max);
+
+      // const expected = arr.slice().sort((a, b) => a - b);
+      // const actual = sortArray(arr.slice());
+      const actual = sortArray(arr);
+      // if (!isSameArray(expected, actual)) {
+      //   console.assert(false, {
+      //     arr,
+      //     expected,
+      //     actual,
+      //   });
+      //   return;
+      // }
+    }
+
+    const timeEnd = Date.now();
+    prevTotalTime = totalTime;
+    totalTime = timeEnd - timeStart;
+    console.log(`N: ${N}, time: ${totalTime}ms, prevTotalTime: ${prevTotalTime}ms`);
+    const ratio = totalTime / prevTotalTime;
+    const bigO = Math.log2(ratio);
+    console.log(`ratio: ${ratio}, bigO: ${bigO}`);
+  }
+}
+
+test();
