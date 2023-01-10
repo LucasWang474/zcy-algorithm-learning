62fcfbd6bc67f048e10ce763c67b893e6ea495e4
Lucas Wang <474.lucas.wang@gmail.com>
2023-01-11 03:10:18 +0800

    feat: add Code02_LC75.ts

---
+++ 01-basic/class05/Code02_LC75.ts
@@ -0,0 +1,31 @@
+/**
+ * https://leetcode.com/problems/sort-colors/description/
+ */
+function sortColors(nums: number[]): void {
+  partition(nums, 1, 0, nums.length - 1);
+}
+
+function partition(nums: number[], pivot: number, lo: number, hi: number) {
+  if (lo >= hi) return [-1, -1];
+
+  let lt = lo - 1,
+    gt = hi + 1,
+    i = lo;
+
+  while (i < gt) {
+    const cur = nums[i];
+
+    if (cur < pivot) {
+      lt++;
+      [nums[i], nums[lt]] = [nums[lt], nums[i]];
+      i++;
+    } else if (cur > pivot) {
+      gt--;
+      [nums[i], nums[gt]] = [nums[gt], nums[i]];
+    } else {
+      i++;
+    }
+  }
+
+  return [lt + 1, gt - 1];
+}
