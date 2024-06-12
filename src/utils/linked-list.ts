937016f18055ef24491713db549a82cef744c1fd
Wenjun <474.lucas.wang@gmail.com>
2024-06-12 22:00:32 +0800

    Update

--- src/utils/linked-list.ts
+++ src/utils/linked-list.ts
@@ -43,3 +43,16 @@ export class MyListNode<T> {
     return res;
   }
 }
+
+/**
+ * LeetCode ListNode
+ */
+export class ListNode {
+  val: number;
+  next: ListNode | null;
+
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
