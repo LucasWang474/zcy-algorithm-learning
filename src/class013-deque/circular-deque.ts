d6d31799194aa96d427c46994e6711ed6ac66699
Wenjun <474.lucas.wang@gmail.com>
2024-06-13 11:22:07 +0800

    Improve circular deque

--- src/class013-deque/circular-deque.ts
+++ src/class013-deque/circular-deque.ts
@@ -1,36 +1,49 @@
 class MyCircularDeque {
   private readonly deque: number[];
-
-  // front element's index
-  private front: number;
-  // the index for putting a new element in the rear
-  private rear: number;
   private size: number;
-  private readonly limit: number;
+  private limit: number;
+  private L: number;
+  private R: number;
 
   constructor(k: number) {
-    this.deque = new Array(k);
-    this.front = 0;
-    this.rear = 0;
-    this.size = 0;
     this.limit = k;
+    this.size = 0;
+    this.L = 0;
+    this.R = 0;
+    this.deque = new Array(k);
+  }
+
+  private insertFirst(value: number) {
+    this.L = 0;
+    this.R = 0;
+    this.deque[0] = value;
+    this.size = 1;
   }
 
   insertFront(value: number): boolean {
     if (this.isFull()) return false;
+    if (this.isEmpty()) {
+      this.insertFirst(value);
+      return true;
+    }
 
-    const newFront = (this.front - 1 + this.limit) % this.limit;
-    this.deque[newFront] = value;
-    this.front = newFront;
+    const newL = (this.L - 1 + this.limit) % this.limit;
+    this.deque[newL] = value;
+    this.L = newL;
     this.size++;
     return true;
   }
 
   insertLast(value: number): boolean {
     if (this.isFull()) return false;
+    if (this.isEmpty()) {
+      this.insertFirst(value);
+      return true;
+    }
 
-    this.deque[this.rear] = value;
-    this.rear = (this.rear + 1) % this.limit;
+    const newR = (this.R + 1) % this.limit;
+    this.deque[newR] = value;
+    this.R = newR;
     this.size++;
     return true;
   }
@@ -38,41 +51,29 @@ class MyCircularDeque {
   deleteFront(): boolean {
     if (this.isEmpty()) return false;
 
-    // if (this.size === 1) {
-    //   this.front = 0;
-    //   this.rear = 0;
-    //   this.size = 0;
-    //   return true;
-    // }
-
     this.size--;
-    this.front = (this.front + 1) % this.limit;
+    this.L = (this.L + 1) % this.limit;
+
     return true;
   }
 
   deleteLast(): boolean {
     if (this.isEmpty()) return false;
 
-    // if (this.size === 1) {
-    //   this.front = 0;
-    //   this.rear = 0;
-    //   this.size = 0;
-    //   return true;
-    // }
-
     this.size--;
-    this.rear = (this.rear - 1 + this.limit) % this.limit;
+    this.R = (this.R - 1 + this.limit) % this.limit;
+
     return true;
   }
 
   getFront(): number {
     if (this.isEmpty()) return -1;
-    return this.deque[this.front];
+    return this.deque[this.L];
   }
 
   getRear(): number {
     if (this.isEmpty()) return -1;
-    return this.deque[(this.rear - 1 + this.limit) % this.limit];
+    return this.deque[this.R];
   }
 
   isEmpty(): boolean {
@@ -83,3 +84,16 @@ class MyCircularDeque {
     return this.size === this.limit;
   }
 }
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * var obj = new MyCircularDeque(k)
+ * var param_1 = obj.insertFront(value)
+ * var param_2 = obj.insertLast(value)
+ * var param_3 = obj.deleteFront()
+ * var param_4 = obj.deleteLast()
+ * var param_5 = obj.getFront()
+ * var param_6 = obj.getRear()
+ * var param_7 = obj.isEmpty()
+ * var param_8 = obj.isFull()
+ */
