97a6ce5f78248688c8c15f297c523b79610ec6c2
Wenjun <474.lucas.wang@gmail.com>
2024-06-20 23:13:51 +0800

    Add 381. Insert Delete GetRandom O(1) - Duplicates allowed

---
+++ src/class035-design-related/insert-delete-getRandom-O1-duplicates-allowed.ts
@@ -0,0 +1,47 @@
+// https://leetcode.com/problems/insert-delete-getrandom-o1-duplicates-allowed/
+class RandomizedCollection {
+  public valueToIndexMap = new Map<number, Set<number>>();
+  public arr: number[] = [];
+
+  constructor() {}
+
+  insert(val: number): boolean {
+    let res;
+
+    this.arr.push(val);
+    if (this.valueToIndexMap.has(val)) {
+      this.valueToIndexMap.get(val)?.add(this.arr.length - 1);
+      res = false;
+    } else {
+      this.valueToIndexMap.set(val, new Set([this.arr.length - 1]));
+      res = true;
+    }
+    return res;
+  }
+
+  remove(val: number): boolean {
+    if (!this.valueToIndexMap.has(val)) return false;
+
+    const setForVal = this.valueToIndexMap.get(val);
+    const indexForVal = setForVal?.values().next().value as number;
+    setForVal?.delete(indexForVal);
+    if (!setForVal?.size) {
+      this.valueToIndexMap.delete(val);
+    }
+
+    if (indexForVal !== this.arr.length - 1) {
+      this.arr[indexForVal] = this.arr[this.arr.length - 1];
+      const setForSwappedVal = this.valueToIndexMap.get(this.arr[indexForVal]);
+      setForSwappedVal?.delete(this.arr.length - 1);
+      setForSwappedVal?.add(indexForVal);
+    }
+
+    this.arr.pop();
+
+    return true;
+  }
+
+  getRandom(): number {
+    return this.arr[Math.trunc(Math.random() * this.arr.length)];
+  }
+}
