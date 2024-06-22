2c073d0de83b808b02a99b92dabb2580a363f0f2
Wenjun <474.lucas.wang@gmail.com>
2024-06-22 21:50:32 +0800

    Add isValidBST

---
+++ src/class037-binary-tree-part2/Code05_ValidateBinarySearchTree.ts
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/validate-binary-search-tree/
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function isValidBST(root: TreeNode | null): boolean {
+  return isValidBSTRecur(root, -Infinity, Infinity);
+}
+
+function isValidBSTRecur(root: TreeNode | null, lower: number, upper: number): boolean {
+  if (!root) return true;
+  if (root.val <= lower || root.val >= upper) return false;
+  if (!root.left && !root.right) return true;
+
+  if (root.left && root.left.val >= root.val) return false;
+  if (root.right && root.right.val <= root.val) return false;
+
+  return (
+    isValidBSTRecur(root.left, lower, root.val) && isValidBSTRecur(root.right, root.val, upper)
+  );
+}
