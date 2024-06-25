fda74f7aa8bb9a650effa2f0d3e77f375ed4555b
Wenjun <474.lucas.wang@gmail.com>
2024-06-25 19:44:58 +0800

    Improve findSubstringInWraproundString

--- src/class066-dp001/Code07_UniqueSubstringsWraparoundString.ts
+++ src/class066-dp001/Code07_UniqueSubstringsWraparoundString.ts
@@ -1,23 +1,22 @@
 // https://leetcode.com/problems/unique-substrings-in-wraparound-string/description/
 
 function findSubstringInWraproundString(s: string): number {
-  const charCodeIdxToCount = new Array(26).fill(0);
-  charCodeIdxToCount[s.charCodeAt(0) - 'a'.charCodeAt(0)] = 1;
-  const dp = new Array(s.length).fill(1);
+  const dp = new Array(26).fill(0);
+  dp[s.charCodeAt(0) - 'a'.charCodeAt(0)] = 1;
 
-  for (let i = 1; i < s.length; i++) {
+  for (let i = 1, length = 1; i < s.length; i++) {
     const curChar = s[i];
     const curCharIdx = curChar.charCodeAt(0) - 'a'.charCodeAt(0);
     const prevCharIdx = s[i - 1].charCodeAt(0) - 'a'.charCodeAt(0);
 
     if ((curCharIdx - 1 + 26) % 26 === prevCharIdx) {
-      dp[i] = dp[i - 1] + 1;
+      length += 1;
     } else {
-      dp[i] = 1;
+      length = 1;
     }
 
-    charCodeIdxToCount[curCharIdx] = Math.max(dp[i], charCodeIdxToCount[curCharIdx]);
+    dp[curCharIdx] = Math.max(length, dp[curCharIdx]);
   }
 
-  return charCodeIdxToCount.reduce((prev, cur) => prev + cur);
+  return dp.reduce((prev, cur) => prev + cur);
 }
